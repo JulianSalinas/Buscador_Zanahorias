940b0d05495a2bd49861ee3b39cfb064b85daaeb
Armando López C <armando12101996@gmail.com>
2018-05-16 16:53:30 -0600

    Funciones para el desplazamiento del conejo

--- tec/ic/ia/pc2/a_star.py
+++ tec/ic/ia/pc2/a_star.py
@@ -5,7 +5,6 @@ import numpy as np
 # -----------------------------------------------------------------------------
 
 def calc_heuristico(pos, metas):
-
     distancias = queue.PriorityQueue()
     for meta in metas:
         distancia = abs(meta[0] - pos[0]) + abs(meta[1] - pos[1])
@@ -16,7 +15,7 @@ def calc_heuristico(pos, metas):
 
 # -----------------------------------------------------------------------------
 
-def get_submatriz(matriz, pos_actual, rango_vision):
+def calc_submatriz(matriz, pos_actual, rango_vision):
     matriz = np.matrix(matriz)
     n_cols = matriz.shape[1]
     n_filas = matriz.shape[0]
@@ -46,19 +45,48 @@ def calc_pos_simbolo(matriz, simbolo):
 
     return indices
 
+
+# -----------------------------------------------------------------------------
+
+def desplazar_conejo(matriz, pos_vieja, pos_nueva, simbolo_vacio='',
+                     simbolo_conejo='C'):
+    pos_x = pos_vieja[0]
+    pos_y = pos_vieja[1]
+    matriz[pos_x, pos_y] = simbolo_vacio
+
+    pos_x = pos_nueva[0]
+    pos_y = pos_nueva[1]
+    matriz[pos_x, pos_y] = simbolo_conejo
+
+    return matriz
+
+
+# -----------------------------------------------------------------------------
+
+def calc_pos_conejo(matriz, simbolo_conejo='C'):
+    pos = []
+    indices = calc_pos_simbolo(matriz, simbolo_conejo)
+
+    if len(indices) > 0:
+        pos = indices[0]
+
+    return pos
+
+
 # -----------------------------------------------------------------------------
 # ------------------ Pruebas --------------------------------------------------
 # -----------------------------------------------------------------------------
+
 test_matrix = [
-    ['Z','','','N','','','Z'],
+    ['','','','N','','','Z'],
     ['','Z','','N','','',''],
-    ['G','','Z','','','C',''],
+    ['G','','Z','','','Z',''],
     ['','','Z','','','',''],
     ['A','B','','Z','','',''],
-    ['Z','','','','','','Z']
+    ['Z','','','','','','C']
 ]
 
-matriz2 = np.arange(50).reshape(5,10)
+matriz2 = np.arange(50).reshape(5, 10)
 
 
 test_matrix = np.matrix(test_matrix)
@@ -69,13 +97,26 @@ for i in range(distancias.qsize()):
     x = distancias.get()
     print('valor: ', x[0], ' - Meta: ', x[1])
 
+
 print('\n---------- Sub Matriz ----------')
 posicion_actual = [4, 1]
 rango_vision = 1
 print('Matriz original: \n', test_matrix)
 print('\nSubMatriz: \n',
-      get_submatriz(test_matrix, posicion_actual, rango_vision))
+      calc_submatriz(test_matrix, posicion_actual, rango_vision))
+
 
 print('\n---------- Posicion de las Zanahorias en la matriz ----------')
 print('Matriz original: \n',test_matrix)
 print('Posiciones:\n', calc_pos_simbolo(test_matrix, 'N'))
+
+
+print('\n---------- Calcular posicion del conejo en la matriz ----------')
+print('Posicion del conejo:\n', calc_pos_conejo(test_matrix))
+
+
+print('\n---------- Desplazar conejo en la matriz ----------')
+pos_vieja = [0, 0]
+pos_nueva = [0, 1]
+print('Movimiento Realizado\n',
+      desplazar_conejo(test_matrix, pos_vieja, pos_nueva))
