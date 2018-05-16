11d72476e44fb75a13dac83e378fd472101fb8ba
Armando López C <armando12101996@gmail.com>
2018-05-16 15:07:46 -0600

    sub matriz con rango de visión

--- tec/ic/ia/pc2/a_star.py
+++ tec/ic/ia/pc2/a_star.py
@@ -14,6 +14,23 @@ def calc_heuristico(pos, metas):
 
 
 # -----------------------------------------------------------------------------
+
+def get_submatriz(matriz, pos_actual, rango_vision):
+    n_cols = len(matriz[0])
+    n_filas = len(matriz)
+
+    x_min = max(0, pos_actual[0] - rango_vision)
+    x_max = min(n_filas, pos_actual[0] + rango_vision + 1)
+
+    y_min = max(0, pos_actual[1] - rango_vision)
+    y_max = min(n_cols, pos_actual[1] + rango_vision + 1)
+
+    sub_matriz = matriz[x_min:x_max, y_min:y_max]
+
+    return sub_matriz
+
+
+# -----------------------------------------------------------------------------
 test_matrix = [
     ['Z', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
@@ -32,14 +49,23 @@ test_matrix = [
     ['', '', 'C', '', '', '', '', '', '', '', '', '', '', 'Z', '']
 ]
 
+matriz2 = np.arange(50).reshape(5,10)
+
+
 test_matrix = np.matrix(test_matrix)
 
 valores = np.nonzero(test_matrix == 'Z')
 
-print(valores[0])
+# print(valores[0])
 
-distancias = calc_heuristico([1, 2], [])
+distancias = calc_heuristico([1, 2], [[0,3], [3,0], [3,1]])
 
+print('---------- Heuristicos ----------')
 for i in range(distancias.qsize()):
     x = distancias.get()
     print('valor: ', x[0], ' - Meta: ', x[1])
+
+print('\n---------- Sub Matriz ----------')
+print('Matriz original: \n',matriz2)
+
+print('\nSubMatriz: \n', get_submatriz(matriz2, [2,7], 1))
