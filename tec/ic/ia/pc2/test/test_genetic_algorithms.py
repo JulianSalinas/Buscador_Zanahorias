b64ca77fde546f4b8cc06d53a3dd29c06702f6bd
Brandon <bdinarte@yahoo.com>
2018-05-22 18:45:56 -0600

    arreglo unittest

--- tec/ic/ia/pc2/test/test_genetic_algorithms.py
+++ tec/ic/ia/pc2/test/test_genetic_algorithms.py
@@ -3,232 +3,111 @@ import numpy as np
 
 from random import seed
 from time import time
+from unittest import TestCase
 
 from model.genetic_algorithms import *
 
 
-def test_gen_in_list():
+class TestGeneticAlgorithms(TestCase):
 
-    gen = Gen(np.array([' ', ' ', 'C', ' ', ' ', ' ', 'Z', 'Z', ' '],
-                       dtype=object))
-    gen2 = Gen(np.array([' ', ' ', 'C', ' ', ' ', ' ', 'Z', 'Z', ' '],
-                        dtype=object))
-    gen3 = Gen(np.array([' ', ' ', 'C', ' ', ' ', ' ', ' ', 'Z', ' '],
-                        dtype=object))
+    def test_gen_in_list(self):
+        """
+        :return: Sin retorno
+        """
+        gen = Gen(np.array([' ', ' ', 'C', ' ', ' ', ' ', 'Z', 'Z', ' '],
+                           dtype=object))
+        gen2 = Gen(np.array([' ', ' ', 'C', ' ', ' ', ' ', 'Z', 'Z', ' '],
+                            dtype=object))
+        gen3 = Gen(np.array([' ', ' ', 'C', ' ', ' ', ' ', ' ', 'Z', ' '],
+                            dtype=object))
 
-    gen_list = [gen2, gen3]
+        gen_list = [gen2, gen3]
 
-    assert gen_in_list(gen, gen_list) == True
+        self.assertTrue(gen_in_list(gen, gen_list))
 
+    def test_direction_to_arrow(self):
+        """
+        :return: Sin retorno
+        """
+        directions = ['arriba', 'abajo', 'izquierda', 'derecha']
+        expected_results = ['A', 'V', '<', '>']
+        real_results = [direction_to_arrow()[d] for d in directions]
 
-def test_direction_to_arrow():
+        self.assertEqual(expected_results, real_results)
 
-    assert direction_to_arrow()['arriba'] == 'A'
-    assert direction_to_arrow()['derecha'] == '>'
+    def test_a_idx_to_m_idx(self):
+        """
+        :return: Sin retorno
+        """
+        matrix_shapes = [(4, 3), (2, 5)]
+        expected_results = [(3, 0), (1, 4)]
+        real_results = [a_idx_to_m_idx(9, ms) for ms in matrix_shapes]
 
+        self.assertEqual(expected_results, real_results)
 
-def test_array_idx_to_matrix_idx():
+    def test_initialization(self):
+        """
+        :return: Sin retorno
+        """
+        matrix = np.matrix([[1, 2, 3],
+                            [4, 5, 6],
+                            [7, 8, 9]])
 
-    matrix_shape = (4, 3)
-    array_index = 9
-    assert a_idx_to_m_idx(array_index, matrix_shape) == (3, 0)
+        generation, matrix_shape = initialization(matrix, 3, 'arriba', False)
 
-    matrix_shape = (2, 5)
-    array_index = 9
-    assert a_idx_to_m_idx(array_index, matrix_shape) == (1, 4)
+        # Se cambia el valor de otro objeto para asegurar que son instancias
+        # aparte
+        generation[1].get_array()[1] = 3
 
+        self.assertEqual(generation[0].get_array()[1], 2)
 
-def test_initialization():
+    def test_cross(self):
+        """
+        :return: Sin retorno
+        """
+        parent1 = Gen(np.array(['A', 'B', 'C', 'D', 'E', 'F', '7', '8', '9']))
+        parent2 = Gen(np.array(['1', '2', '3', '4', '5', '6', 'G', 'H', 'I']))
 
-    matrix = np.matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        seed(7)
 
-    generation, matrix_shape = initialization(matrix, 3, 'arriba', False)
+        childs = cross(parent1, parent2, 1)
 
-    assert len(generation) == 3
-    assert generation[0].get_array()[1] == 2
+        expected_results = list()
+        expected_results.append(['1', '2', '3',
+                                 '4', '5', '6',
+                                 '7', '8', '9'])
 
-    # Se cambia el valor de otro objeto para asegurar que son instancias aparte
-    generation[1].get_array()[1] = 3
+        expected_results.append(['A', 'B', 'C',
+                                 'D', 'E', 'F',
+                                 'G', 'H', 'I'])
 
-    # Pero el valor de la arreglo 0 sigue siendo 2, y el del arreglo 1 es 3
-    assert generation[0].get_array()[1] == 2
-    assert generation[1].get_array()[1] == 3
+        real_results = [c.get_array().tolist() for c in childs]
+        seed(time())
 
+        self.assertEqual(expected_results, real_results)
 
-def test_crossing():
-    """
-    Prueba de la función de cruce para 2 genes, con ambos tipos de cruce
-    definidos
-    :return: None
-    """
-    parent1 = Gen(np.array(['A', 'B', 'C', 'D', 'E', 'F', '7', '8', '9']))
-    parent2 = Gen(np.array(['1', '2', '3', '4', '5', '6', 'G', 'H', 'I']))
+    def test_mutation(self):
+        """
+        :return: Sin retorno
+        """
+        gen = Gen(np.array([' ', ' ', 'C', ' ', ' ', ' ', 'Z', 'Z', ' '],
+                           dtype=object))
 
-    seed(7)
+        seed(1)
+        mutation1, _ = mutate(gen, 20)  # Inserción de una flecha
+        seed(2)
+        mutation2, _ = mutate(gen, 20)  # Inserción de una flecha
+        seed(2)
+        mutation3, _ = mutate(mutation2, 20)  # Cambio de dirección de flecha
 
-    child1, child2 = cross(parent1, parent2, 1)
+        real_results = list()
+        real_results.append(mutation1.get_array().tolist())
+        real_results.append(mutation2.get_array().tolist())
 
-    assert child1.get_array().tolist() == ['1', '2', '3',
-                                           '4', '5', '6',
-                                           '7', '8', '9']
-    assert child2.get_array().tolist() == ['A', 'B', 'C',
-                                           'D', 'E', 'F',
-                                           'G', 'H', 'I']
+        expected_results = list()
+        expected_results.append([' ', 'A', 'C', ' ', ' ', ' ', 'Z', 'Z', ' '])
+        expected_results.append([' ', '<', 'C', ' ', ' ', ' ', 'Z', 'Z', ' '])
 
-    seed(1)
+        self.assertEqual(real_results, expected_results)
 
-    child1, child2 = cross(child1, child2, 2)
-
-    assert child1.get_array().tolist() == ['1', '2', 'C',
-                                           '4', '5', '6',
-                                           '7', '8', '9']
-    assert child2.get_array().tolist() == ['A', 'B', '3',
-                                           'D', 'E', 'F',
-                                           'G', 'H', 'I']
-
-    seed(time())
-
-
-def test_mutation():
-
-    gen = Gen(np.array([' ', ' ', 'C', ' ', ' ', ' ', 'Z', 'Z', ' '],
-                       dtype=object))
-
-    seed(1)
-    mutation1, _ = mutate(gen, 20)  # Inserción de una flecha
-    assert mutation1.get_array().tolist() == [' ', 'A', 'C', ' ', ' ',
-                                              ' ', 'Z', 'Z', ' ']
-
-    seed(2)
-    mutation2, _ = mutate(gen, 20)  # Inserción de una flecha
-    assert mutation2.get_array().tolist() == [' ', '<', 'C', ' ', ' ',
-                                              ' ', 'Z', 'Z', ' ']
-
-    seed(2)
-    mutation3, _ = mutate(mutation2, 20)  # Cambio de dirección de la flecha
-    assert mutation3.get_array().tolist() == [' ', '>', 'C', ' ', ' ',
-                                              ' ', 'Z', 'Z', ' ']
-
-    seed(time())
-
-
-def test_fitness():
-    """
-    Prueba de la función de aptitud con cuatro soluciones diferentes
-    :return: None
-    """
-    test_not_optimal = [
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', 'Z', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '<'],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        ['>', ' ', ' ', ' ', ' ', ' ', 'V', ' ', ' ', ' ', '>', ' ', 'Z', 'A'],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', '>', ' ', ' ', ' ', 'A', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        ['A', ' ', '<', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', 'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
-
-    test_not_optimal = Gen((np.matrix(test_not_optimal, dtype=object)).getA1())
-    eval_fitness(test_not_optimal, 'arriba', (15, 14))
-
-    test_optimal = [
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        ['V', 'Z', '<', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        ['>', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'Z', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', 'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
-
-    test_optimal = Gen((np.matrix(test_optimal, dtype=object)).getA1())
-    eval_fitness(test_optimal, 'arriba', (15, 14))
-
-    assert test_not_optimal.get_score() < test_optimal.get_score()
-
-
-def test_creating_generation():
-
-    seed(2018)
-
-    test_matrix = [
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', 'Z', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'Z', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', 'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
-
-    test_matrix = np.matrix(test_matrix, dtype=object)
-    first_generation, shape = initialization(initial_board=test_matrix,
-                                             individuals=5,
-                                             direction='arriba')
-    new_generation = generate(first_generation, 1, 'arriba', shape,
-                              mutation_chance=100, cross_type=1)
-
-    assert len(new_generation) == 9
-
-    seed(time())
-
-
-def test_generation_replacement():
-
-    seed(2018)
-
-    test_matrix = [
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', 'Z', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'Z', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', 'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
-    individuals = 5
-
-    test_matrix = np.matrix(test_matrix, dtype=object)
-    first_generation, shape = initialization(initial_board=test_matrix,
-                                             individuals= individuals,
-                                             direction='arriba')
-
-    assert len(first_generation) == individuals
-
-    untrimmed_generation = generate(first_generation, 1, 'arriba', shape,
-                                    mutation_chance=100, cross_type=1)
-
-    assert len(untrimmed_generation) == 9
-
-    second_generation = replacement(untrimmed_generation, individuals)
-
-    assert len(second_generation) == individuals
-
-    seed(time())
+        seed(time())
